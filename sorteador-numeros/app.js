c708fb1099af0d84537e5c1c65fb92e8b8a14c6c
devanaclimgo <anaclimgo@gmail.com>
2024-10-23 12:15:10 -0300

    função para sortear add

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,7 +3,17 @@ function sortear() {
   let de = parseInt(document.getElementById('de').value)
   let ate = parseInt(document.getElementById('ate').value)
 
-  alert(`Quantidade: ${quantidade}`)
-  alert(`Do número: ${de}`)
-  alert(`Até o número: ${ate}`)
+  let sorteados = []
+  let numero
+
+  for (let i = 0; i < quantidade; i++) {
+    numero = obterNumeroAleatorio(de, ate)
+    sorteados.push(numero)
+  }
+
+  alert(sorteados)
+}
+
+function obterNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
